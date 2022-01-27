dc0b2e19d12860b9577491cbe53cc74fa5949916
Yash Patel <yashhp@ad.unc.edu>
2022-01-27 12:34:06 -0500

    new work on eightball

---
+++ lessons/eightball.py
@@ -0,0 +1,16 @@
+"""An oracle that predicts the future."""
+
+from random import randint
+
+input("Ask a yes/no question: ")
+
+response: int = randint(0, 3)
+
+if response == 0:
+    print("Most definitely")
+elif response == 1:
+    print("Most likely")
+elif response == 2:
+    print("Ask again later")
+else: 
+    print("No way, not a chance")
