e99a88a42ac90f0dedceffefd99ba5968267ec72
Yash Patel <yashhp@ad.unc.edu>
2022-02-20 18:45:18 -0500

    EX05 Progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,50 @@
+"""EX05 - 'list' Utility Functions."""
+
+__author__ = "730463236"
+
+
+def only_evens(num: list[int]) -> list[int]:
+    """This function identifies the even numbers in a list and seperates it from the remainder of the list."""
+    num2: list[int] = list()
+    i: int = 0
+
+    while i < len(num):
+        if num[i] % 2 == 0:
+            num2.append(num[i])
+            i += 1
+    return num2
+    
+
+def sub(num: list[int], first_index: int, end_index: int) -> list[int]:
+    """This function generates a subset between the requested indexes of the provided list."""
+    num2: list[int] = list()
+    end_index = end_index - 1
+
+    if first_index < 0:
+        first_index = 0
+    if end_index >= len(num):
+        end_index = len(num) - 1
+    if len(num) == 0 or first_index > len(num) or end_index <= 0:
+        num2: list[int] = []
+
+    while first_index <= end_index:
+        num2.append(num[first_index])
+        first_index += 1
+    return num2
+
+
+def concat(num_evens: list[int], num_sub: list[int]) -> list[int]:
+    """This function concatenates the sub and only_evens functions."""
+    num_concat: list[int] = list()
+
+    i: int = 0 
+    while i < len(num_evens):
+        num_concat.append(num_evens[i])
+        i += 1
+    
+    i: int = 0
+    while i < len(num_sub):
+        num_concat.append(num_sub[i])
+        i += 1
+    return num_concat
+    
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,71 @@
+"""Tests for util programs."""
+
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub 
+from exercises.ex05.utils import concat
+
+
+"""Testing the only_evens function."""
+
+
+def test_only_evens_empty() -> None:
+    """Testing the only_evens function with an empty string."""
+    num: list[int] = []
+    assert only_evens(num) == []
+
+
+def test_only_evens_less() -> None:
+    """Testing the only_evens function with a small string."""
+    num: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(num) == [2, 4, 6]
+
+
+def test_only_evens_more() -> None:
+    """Testing the only_evens function with a larger string."""
+    num: list[int] = [2, 2, 4, 4, 6, 6, 8, 8]
+    assert only_evens(num) == [2, 2, 4, 4, 6, 6, 8, 8]
+
+
+"""Testing the sub function."""
+
+
+def test_sub_empty() -> None: 
+    """Testing the sub function with an empty string."""
+    num: list[int] = []
+    assert sub(num, 0, 5) == []
+
+
+def test_sub_less() -> None: 
+    """Testing the sub function with a small string."""
+    num: list[int] = [5, 10, 15, 20, 25]
+    assert sub(num, 2, 5) == [15, 20, 25]
+
+
+def test_sub_more() -> None: 
+    """Testing the sub function with a larger string."""
+    num: list[int] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    assert sub(num, -1, 7) == [10, 20, 30, 40, 50, 60, 70]
+
+
+"""Testing the concat function."""
+
+
+def test_concat_empty() -> None:
+    """Testing the concat function with an empty string."""
+    num_evens: list[int] = [10, 15, 20]
+    num_sub: list[int] = []
+    assert concat(num_evens, num_sub) == [10, 15, 20]
+
+
+def test_concat_less() -> None: 
+    """Testing the concat function with a small string."""
+    num_evens: list[int] = [1, 3, 4, 7]
+    num_sub: list[int] = [3, 5, 6, 8]
+    assert concat(num_evens, num_sub) == [1, 3, 4, 7, 3, 5, 6, 8]
+
+
+def test_concat_more() -> None: 
+    """Testing the concat function with a larger string."""
+    num_evens: list[int] = [10, 20, 25, 30, 35, 50, 80]
+    num_sub: list[int] = [5, 15, 32, 48, 65, 75, 95, 99, 100]
+    assert concat(num_evens, num_sub) == [10, 20, 25, 30, 35, 50, 80, 5, 15, 32, 48, 65, 75, 95, 99, 100]
